cee3aaa4829d9e59585afea70a61892b0c2115c6
robisa693 <roberto_isaac@live.se>
2020-01-11 20:51:12 +0100

    Version 0.0.2 added menu and decrement ability

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import sqlite3
 import time
 from sqlite3 import Error
 
-
-
 def sql_connection():
     try:
         connection = sqlite3.connect('mydatabase.db')
@@ -32,7 +30,8 @@ def add_item_to_inventory_table(con, tablename, date, name):
                   VALUES('{name.lower()}','{date}', 0) '''
     sql_cursor.execute(sql)
     con.commit()
-def increment_count_of_item(con, name, table_name):
+
+def increment_count_of_item(con, name, table_name, subtract=False):
     sql_cursor = con.cursor()
     id = None
     new_count = None
@@ -44,7 +43,12 @@ def increment_count_of_item(con, name, table_name):
             id = row[0]
             current_count = row[2]
     if id != None:
-        new_count = current_count + 1
+        if subtract == True and current_count > 0:
+            new_count = current_count - 1
+        elif subtract == False:
+            new_count = current_count + 1
+        else:
+            print("Could not increment/subtract. Item might already be 0")
         new_date = str(time.strftime("%Y-%m-%d %H:%M"))
         sql_update_query = f"""Update {table_name} set count = {new_count}, update_date = '{new_date}' where id = {id}"""
         sql_cursor.execute(sql_update_query)
@@ -52,6 +56,7 @@ def increment_count_of_item(con, name, table_name):
     else:
         print("item not found")
     con.commit()
+
 #main
 def main():
     date = str(time.strftime("%Y-%m-%d %H:%M"))
@@ -66,14 +71,35 @@ def main():
                 UNIQUE(name)
                 );"""
     create_sql_table(con, table)
-    item_name = input("Name of new item: ")
-    if item_name != "":
-        add_item_to_inventory_table(con, tablename, date, item_name)
-    print_sql_table(con, tablename)
+    loop = True
+    while loop:
 
-    item_name = input("name of item to increment: ")
-    if item_name != "":
-        increment_count_of_item(con, item_name, tablename)
+        print("""What would you like to do?
+        1.Add new item
+        2.Increment item
+        3.Decrement item
+        4.Print table
+        5.Exit
+        """)
+
+        inputs = input('answer: ')
+        if inputs == "1":
+            item_name = input("Name of new item: ")
+            add_item_to_inventory_table(con, tablename, date, item_name)
+            print_sql_table(con, tablename)
+
+        if inputs == "2":
+            item_name = input("name of item to increment: ")
+            increment_count_of_item(con, item_name, tablename)
+            print_sql_table(con, tablename)
+
+        if inputs == "3":
+            item_name = input("name of item to decrement: ")
+            increment_count_of_item(con, item_name, tablename, subtract=True)
+        if inputs == "4":
+            print_sql_table(con, tablename)
+        if inputs == "5":
+            loop=False
 
     con.commit()
     print_sql_table(con, tablename)
